8ba3f912c185ab07d588c01f5fb1a4b0600fabf5
mac <mac@macdeAir.hitronhub.home>
2020-05-27 14:59:46 -0700

    Updates

---
+++ precache-manifest.ef9d0b811ba0a81d93a5f73d76479e15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc3d89adc58ed6be5bc9eae96787c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa05abd392c930687e6",
+    "url": "/static/css/2.3d314d19.chunk.css"
+  },
+  {
+    "revision": "76370af9fb900c32ccff",
+    "url": "/static/css/main.d8183793.chunk.css"
+  },
+  {
+    "revision": "caa05abd392c930687e6",
+    "url": "/static/js/2.9c2ceb72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9c2ceb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76370af9fb900c32ccff",
+    "url": "/static/js/main.f175f52e.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  }
+]);
